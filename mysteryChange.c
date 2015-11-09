60967049b335ecdeddb4af4a03cf843b34b66332
Anupam02 <patel.anupam02@gmail.com>
2015-11-09 23:47:45 +0530

    initial

---
+++ mysteryChange.c
@@ -0,0 +1,21 @@
+// Copyright 2015
+/** Here is a program that violates array bounds (which C doesn't detect) **/
+# include <stdio.h>
+# include <stdlib.h>
+# include <assert.h>
+
+int main(int argc, char *argv[])
+{
+  int x;
+  int a[10];
+  int i;
+
+  x = 5;
+
+  for (i = -1; i < 11; i++) {
+    a[i] = 37;
+  }
+  assert(x == 5);
+  
+  return 0;
+}
---
+++ uninitialized.c
@@ -0,0 +1,17 @@
+// Copyright 2015
+/** program to illustrate use of valgrid **/
+# include <stdio.h>
+# include <string.h>
+
+int main(int argc, char *argv[])
+{
+  char a[2];
+
+  a[0] = 'a';
+
+  if (!strcmp(a, "a")) {
+    puts(" a is \"a\"");
+  }
+  
+  return 0;
+}
